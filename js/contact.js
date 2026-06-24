eb0a7771091c99e623860fa6b1ea88b30c4b74be
agboji-simon <agbojisimon107@gmail.com>
2026-06-24 03:19:56 -0700

    Initial project structure

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+/*
+  js/contact.js
+  Purpose: Placeholder for contact page JavaScript. Will handle form validation and submission logic later.
+*/
---
+++ js/planner.js
@@ -0,0 +1,6 @@
+/*
+  js/planner.js
+  Purpose: Placeholder for planner page JavaScript. Will contain logic to manage courses,
+  assignments, deadlines and calendar interactions in the future.
+*/
+
